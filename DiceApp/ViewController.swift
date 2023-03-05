d2875fe3bdf23b2b7fe3ae693fd53f17aafd95d8
Ümit Şimşek <umitsimsek0@icloud.com>
2023-03-05 16:37:03 +0300

    Initial Commit

---
+++ DiceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceApp
+//
+//  Created by Ümit Şimşek on 5.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
