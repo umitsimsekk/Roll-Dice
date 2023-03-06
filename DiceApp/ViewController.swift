55e2e9bdbd3f72f9ba63fd94a76e0be226b5a936
Ümit Şimşek <umitsimsek002@gmail.com>
2023-03-06 15:02:32 +0300

    application completed

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgSecond: UIImageView!
+    @IBOutlet weak var imgFirst: UIImageView!
+    
+    var diceImgArray : Array<UIImage> = [
+        UIImage(named: "dice1")!,
+        UIImage(named: "dice2")!,
+        UIImage(named: "dice3")!,
+        UIImage(named: "dice4")!,
+        UIImage(named: "dice5")!,
+        UIImage(named: "dice6")!,
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func rollDiceClicked(_ sender: Any) {
+        let rndmNumberFirst =  arc4random_uniform(6)
+        let rndmNumberSecond =  arc4random_uniform(6)
+        
+        imgFirst.image = diceImgArray[Int(rndmNumberFirst)]
+        imgSecond.image = diceImgArray[Int(rndmNumberSecond)]
 
+    }
+    
 }
 
